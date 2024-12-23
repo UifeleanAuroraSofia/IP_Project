a1df9fed89dad40d26c0d586d8365b405add25d0
UifeleanAuroraSofia <92993453+UifeleanAuroraSofia@users.noreply.github.com>
2024-12-23 13:13:23 +0200

    Update server.c

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <string.h>
 #include "client.h"
 #define SA struct sockaddr 
 #define PORT 22228
@@ -13,7 +14,7 @@ int main(void) {
 
 	struct sockaddr_storage their_addr;
 	struct sockaddr_in server_addr;
-	int sockfd, new_socket, result, trimitere;
+	int sockfd, new_socket, result, trimitere, comanda;
 	char buffer[500];
 	socklen_t addr_size;
         char *msg = "Comanda neimplementata";
@@ -44,44 +45,39 @@ int main(void) {
    	} 
    	 else
        		 printf("Server listening..\n");  
-
       do {
 		 addr_size = sizeof their_addr;
                  new_socket = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
 
 	 	 if (new_socket == -1) {
 			perror("accept()");
-
-			
-	        }
+	         }
 		printf("\nConexiune reusita");
 		
 		do {
-			result = recv(new_socket, buffer, 500, 0);
-			if (result > 0) {
-				
-				printf("\nComanda introdusa: %.*s\n", result,  buffer);
+                        result = recv(new_socket, buffer, 500, 0);
+			buffer[3] = '\0';
+			if (strcmp("08#", buffer) == 0) {
+				ClientMessage(new_socket);
+			}
+			else if (strcmp("08#", buffer) != 0) {
+				 trimitere = send(new_socket, msg, strlen(msg), 0);
+	               		 if (trimitere == -1){
+		       			 perror("send()");
+					 exit(0);
+				 }
 			}
 			else if (result == 0) {
 				printf("\nConexiune inchisa");
 			}
 			else {
 				printf("\nConexiune esuata");
-			} 
-
-		
-              	        trimitere = send(new_socket, msg, strlen(msg), 0);
-	                if (trimitere == -1){
-		       		 perror("send()");
-				 exit(0);
-			}
-			else {
-				printf("\nNumarul de octeti trimisi: %d ",trimitere);
 			}
 
 	   	}while(result > 0);
-		close(new_socket);
 	
 	}while(new_socket != -1);
+      close(new_socket);
+      close(sockfd);
 	
 }
